5963bcf6a0caa620eb755fc8ed13ed2ba61e7ede
gabrielagustin <gabiagus@gmail.com>
2019-07-23 11:19:31 -0300

    NN implementation

--- nn_using_keras.py
+++ nn_using_keras.py
@@ -4,5 +4,17 @@ Created on Mon Jul 22 16:16:04 2019
 
 @author: gag
 
-
 """
+
+import pandas as pd
+
+path = "/media/gag/Datos/Estancia_Italia_2018/Trabajo_Sentinel_NDVI_CONAE/Modelo/"
+
+fileTraining = "DataSet_Training_1km.csv"
+fileTest = "DataSet_Test_1km.csv"
+data = pd.read_csv(path + fileTraining, sep=',', decimal=",")
+print ("Numero muestras training: " + str(len(data)))
+
+
+dataTest = pd.read_csv(path + fileTest, sep=',', decimal=",")
+print ("Numero muestras test: " + str(len(dataTest)))
