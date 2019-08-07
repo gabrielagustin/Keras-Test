9812b8576389604d1a57842714602dbc9b9d33ce
gabrielagustin <gabiagus@gmail.com>
2019-08-07 11:20:59 -0300

    improving code

--- grid_Search_Keras.py
+++ grid_Search_Keras.py
@@ -10,7 +10,7 @@ Use scikit-learn to grid search to find the best parameters
 
 """
 
-import numpy
+import numpy as np
 from sklearn.model_selection import GridSearchCV
 from keras.models import Sequential
 from keras.layers import Dense
@@ -18,28 +18,70 @@ from keras.wrappers.scikit_learn import KerasClassifier
 
 
 import nn_Model_Keras
+import lectura
+
+path = "/home/gag/MyProjects/Keras-Test/"
+
+fileTraining = "DataSet_Training_1km.csv"
+fileTest = "DataSet_Test_1km.csv"
+
+### OJO!! ambos conjuntos de datos estan normalizados utilizando sus valores 
+### maximos y minimos relativos, NO del set completo. MEJORAR!!!
+
+dataTraining = lectura.read(path + fileTraining)
+print(list(dataTraining.columns))
+print ("Numero muestras training: " + str(len(dataTraining)))
+
+dataTest = lectura.read(path + fileTest)
+print ("Numero muestras test: " + str(len(dataTest)))
+
+
+### se separan ambos conjuntos de entranamiento y prueba, en entrada (X) y salida (y)
+y_train = np.array(dataTraining["SM_SMAP"])
+# y_train = 10**(y_train)
+del dataTraining["SM_SMAP"]
+X_train = dataTraining
+
+y_test = np.array(dataTest["SM_SMAP"])
+# y_test = 10**(y_test)
+del dataTest["SM_SMAP"]
+X_test = dataTest
 
 
 # fix random seed for reproducibility
 seed = 7
-numpy.random.seed(seed)
-# load dataset
-dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
-# split into input (X) and output (Y) variables
-X = dataset[:,0:8]
-Y = dataset[:,8]
+np.random.seed(seed)
+
 # create model
-model = KerasClassifier(build_fn=create_model, verbose=0)
+snn_model = nn_Model_Keras.create_simple_nn()  
+snn_model.compile(optimizer='adam', loss='mean_absolute_error', metrics=['mean_squared_error'])  
 # define the grid search parameters
 batch_size = [10, 20, 40, 60, 80, 100]
 epochs = [10, 50, 100]
 param_grid = dict(batch_size=batch_size, epochs=epochs)
-grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
-grid_result = grid.fit(X, Y)
-# summarize results
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
+# grid search
+grid = GridSearchCV(estimator=snn_model, param_grid=param_grid)
+grid_result = grid.fit(X_train, y_train)
+
+# # summarize results
+# print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+# means = grid_result.cv_results_['mean_test_score']
+# stds = grid_result.cv_results_['std_test_score']
+# params = grid_result.cv_results_['params']
+# for mean, stdev, param in zip(means, stds, params):
+#     print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
